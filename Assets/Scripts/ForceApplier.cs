bd56facf17e5df4d54baf951c490091ece69f09b
Robert Lesser <rdlesser@gmail.com>
2022-12-23 17:58:01 +0200

    WIP

--- Assets/Scripts/ForceApplier.cs
+++ Assets/Scripts/ForceApplier.cs
@@ -4,11 +4,13 @@ public class ForceApplier : MonoBehaviour
 {
 	[SerializeField] private Rigidbody2D _rigidBody;
 	[SerializeField] private float _forceStrength = 100f;
+	[SerializeField] private Vector2 _xDirectionRange;
+	[SerializeField] private Vector2 _yDirectionRange;
 	
 	public void ApplyRandomForce()
 	{
-		var xDirection = Random.Range(-1f, 1f);
-		var yDirection = Random.Range(-1f, 1f);
+		var xDirection = Random.Range(_xDirectionRange.x, _xDirectionRange.y);
+		var yDirection = Random.Range(_yDirectionRange.x, _yDirectionRange.y);
 
 		var directionVector = new Vector2(xDirection, yDirection);
 		directionVector.Normalize();
