56dcf3e9b60f27fc59fdf7acab397f7ae252e516
Robert Lesser <rdlesser@gmail.com>
2022-12-23 19:48:45 +0200

    damage manager start

---
+++ Assets/Scripts/DamageManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageManager : MonoBehaviour
+{
+
+	[SerializeField] private Vector2 _damage1Range;
+	[SerializeField] private Vector2 _damage2Range;
+	[SerializeField] private Vector2 _damage3Range;
+	[SerializeField] private Slider _slider;
+
+	private Coroutine _sliderCoroutine;
+	
+	private void OnEnable()
+	{
+		StartTimer();
+	}
+
+	private void StartTimer()
+	{
+		_sliderCoroutine = StartCoroutine(DecreaseDamage());
+	}
+
+	private IEnumerator DecreaseDamage()
+	{
+		while (_slider.value > 0)
+		{
+			if (_slider.value - 5 <= 0)
+			{
+				_slider.value = 0;
+				StopCoroutine(_sliderCoroutine);
+				//TODO: Event for game finish
+			}
+			
+			yield return new WaitForSeconds(0.3f);
+			_slider.value -= 5;
+		}
+	}
+}
