90607ead57ae4be151807404b8d60623c79fd0bc
Robert Lesser <rdlesser@gmail.com>
2022-12-24 23:52:26 +0200

    intro sound

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	[SerializeField] private AudioSource _intro;
+
+	private void Start()
+	{
+		_intro.Play();
+	}
+}
